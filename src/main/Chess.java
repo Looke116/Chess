caaef1a8ebc8c8c953a1a195cbda30177328ea78
Tanasa Alex <talexmihai@yahoo.com>
2019-03-27 18:34:39 +0200

    v0.2
    still cant move but now there are more

--- src/main/Chess.java
+++ src/main/Chess.java
@@ -4,12 +4,17 @@ import processing.core.PApplet;
 
 public class Chess extends PApplet {
 
-	static int CANVAS_WIDTH = 400;
-	static int CANVAS_HEIGHT = 400;
-	static int LENGTH = 50;
+	final int CANVAS_WIDTH = 400;
+	final int CANVAS_HEIGHT = 400;
+	final int LENGTH = 50;
 	int ROWS = 8;
 	int COLUMNS = 8;
 	Piece[] pawn = new Pawn[16];
+	Piece[] rook = new Rook[4];
+	Piece[] knight = new Knight[4];
+	Piece[] bishop = new Bishop[4];
+	Piece[] queen = new Queen[2];
+	Piece[] king = new King[2];
 
 	public static void main(String[] args) {
 		PApplet.main("main.Chess");
@@ -20,16 +25,45 @@ public class Chess extends PApplet {
 	}
 
 	public void setup() {
-		for (int i = 0; i < 16; i++) {
+		// creating the pieces
+		//// pawn
+		for (int i = 0; i < pawn.length; i++) {
 			if (i < 8) {
 				pawn[i] = new Pawn(i, 1, LENGTH, false, this);
 			} else {
 				pawn[i] = new Pawn(i - 8, 6, LENGTH, true, this);
 			}
 		}
+
+		//// rook
+		rook[0] = new Rook(0, 0, LENGTH, false, this);
+		rook[1] = new Rook(7, 0, LENGTH, false, this);
+		rook[2] = new Rook(0, 7, LENGTH, true, this);
+		rook[3] = new Rook(7, 7, LENGTH, true, this);
+
+		//// knight
+		knight[0] = new Knight(1, 0, LENGTH, false, this);
+		knight[1] = new Knight(6, 0, LENGTH, false, this);
+		knight[2] = new Knight(1, 7, LENGTH, true, this);
+		knight[3] = new Knight(6, 7, LENGTH, true, this);
+
+		//// bishop
+		bishop[0] = new Bishop(2, 0, LENGTH, false, this);
+		bishop[1] = new Bishop(5, 0, LENGTH, false, this);
+		bishop[2] = new Bishop(2, 7, LENGTH, true, this);
+		bishop[3] = new Bishop(5, 7, LENGTH, true, this);
+
+		//// queen
+		queen[0] = new Queen(3, 0, LENGTH, false, this);
+		queen[1] = new Queen(3, 7, LENGTH, true, this);
+
+		//// king
+		king[0] = new King(4, 0, LENGTH, false, this);
+		king[1] = new King(4, 7, LENGTH, true, this);
 	}
 
 	public void draw() {
+		// creating the board
 		for (int i = 0; i < ROWS; i++) {
 			for (int j = 0; j < COLUMNS; j++) {
 				noStroke();
@@ -43,15 +77,94 @@ public class Chess extends PApplet {
 			}
 		}
 
-		for (int i = 0; i < 16; i++) {
-			((main.Pawn) pawn[i]).draw();
+		// drawing the pieces
+		//// pawn
+		for (int i = 0; i < pawn.length; i++) {
+			((Pawn) pawn[i]).draw();
+		}
+
+		//// rook
+		for (int i = 0; i < rook.length; i++) {
+			((Rook) rook[i]).draw();
+		}
+
+		//// knight
+		for (int i = 0; i < knight.length; i++) {
+			((Knight) knight[i]).draw();
+		}
+
+		//// bishop
+		for (int i = 0; i < bishop.length; i++) {
+			((Bishop) bishop[i]).draw();
+		}
+
+		//// queen
+		for (int i = 0; i < queen.length; i++) {
+			((Queen) queen[i]).draw();
+		}
+
+		//// king
+		for (int i = 0; i < king.length; i++) {
+			((King) king[i]).draw();
 		}
 	}
 
 	public void mousePressed() {
+		// putting the selected piece back down
+		//// pawn
+		for (int i = 0; i < pawn.length; i++) {
+			if (pawn[i].moving) {
+				pawn[i].moving = false;
+				// TODO figure out a way to change i and j
+			}
+		}
+
+		//// rook
+		//// knight
+		//// bishop
+		//// queen
+		//// king
+
+		// checking for selected piece
+		//// pawn
 		for (int i = 0; i < pawn.length; i++) {
 			if (pawn[i].checkMouseLocation(mouseX, mouseY)) {
 				pawn[i].moving = true;
+			}
+		}
+
+		//// rook
+		for (int i = 0; i < rook.length; i++) {
+			if (rook[i].checkMouseLocation(mouseX, mouseY)) {
+				rook[i].moving = true;
+			}
+		}
+
+		//// knight
+		for (int i = 0; i < knight.length; i++) {
+			if (knight[i].checkMouseLocation(mouseX, mouseY)) {
+				knight[i].moving = true;
+			}
+		}
+
+		//// bishop
+		for (int i = 0; i < bishop.length; i++) {
+			if (bishop[i].checkMouseLocation(mouseX, mouseY)) {
+				bishop[i].moving = true;
+			}
+		}
+
+		//// queen
+		for (int i = 0; i < queen.length; i++) {
+			if (queen[i].checkMouseLocation(mouseX, mouseY)) {
+				queen[i].moving = true;
+			}
+		}
+
+		//// king
+		for (int i = 0; i < king.length; i++) {
+			if (king[i].checkMouseLocation(mouseX, mouseY)) {
+				king[i].moving = true;
 			}
 		}
 	}
--- src/main/Piece.java
+++ src/main/Piece.java
@@ -23,7 +23,12 @@ public class Piece {
 		moving = false;
 		parent = p;
 	}
-	
+
+	void setIndex(int i, int j) {
+		this.i = i;
+		this.j = j;
+	}
+
 	boolean checkMouseLocation(int x, int y) {
 		return (x > this.x && x < this.x + length && y > this.y && y < this.y + length);
 	}
@@ -52,6 +57,146 @@ class Pawn extends Piece {
 				parent.image(whitePawn, x, y, length, length);
 			} else {
 				parent.image(blackPawn, x, y, length, length);
+			}
+		}
+	}
+}
+
+class Rook extends Piece {
+
+	PImage blackRook;
+	PImage whiteRook;
+
+	public Rook(int i, int j, int length, boolean white, PApplet p) {
+		super(i, j, length, white, p);
+		blackRook = parent.loadImage("Image\\Black\\Rook.png");
+		whiteRook = parent.loadImage("Image\\White\\Rook.png");
+	}
+
+	public void draw() {
+		if (moving) {
+			if (white) {
+				parent.image(whiteRook, parent.mouseX - length / 2, parent.mouseY - length / 2, length, length);
+			} else {
+				parent.image(blackRook, parent.mouseX - length / 2, parent.mouseY - length / 2, length, length);
+			}
+		} else {
+			if (white) {
+				parent.image(whiteRook, x, y, length, length);
+			} else {
+				parent.image(blackRook, x, y, length, length);
+			}
+		}
+	}
+}
+
+class Knight extends Piece {
+
+	PImage blackKnight;
+	PImage whiteKnight;
+
+	public Knight(int i, int j, int length, boolean white, PApplet p) {
+		super(i, j, length, white, p);
+		blackKnight = parent.loadImage("Image\\Black\\Knight.png");
+		whiteKnight = parent.loadImage("Image\\White\\Knight.png");
+	}
+
+	public void draw() {
+		if (moving) {
+			if (white) {
+				parent.image(whiteKnight, parent.mouseX - length / 2, parent.mouseY - length / 2, length, length);
+			} else {
+				parent.image(blackKnight, parent.mouseX - length / 2, parent.mouseY - length / 2, length, length);
+			}
+		} else {
+			if (white) {
+				parent.image(whiteKnight, x, y, length, length);
+			} else {
+				parent.image(blackKnight, x, y, length, length);
+			}
+		}
+	}
+}
+
+class Bishop extends Piece {
+
+	PImage blackBishop;
+	PImage whiteBishop;
+
+	public Bishop(int i, int j, int length, boolean white, PApplet p) {
+		super(i, j, length, white, p);
+		blackBishop = parent.loadImage("Image\\Black\\Bishop.png");
+		whiteBishop = parent.loadImage("Image\\White\\Bishop.png");
+	}
+
+	public void draw() {
+		if (moving) {
+			if (white) {
+				parent.image(whiteBishop, parent.mouseX - length / 2, parent.mouseY - length / 2, length, length);
+			} else {
+				parent.image(blackBishop, parent.mouseX - length / 2, parent.mouseY - length / 2, length, length);
+			}
+		} else {
+			if (white) {
+				parent.image(whiteBishop, x, y, length, length);
+			} else {
+				parent.image(blackBishop, x, y, length, length);
+			}
+		}
+	}
+}
+
+class Queen extends Piece {
+
+	PImage blackQueen;
+	PImage whiteQueen;
+
+	public Queen(int i, int j, int length, boolean white, PApplet p) {
+		super(i, j, length, white, p);
+		blackQueen = parent.loadImage("Image\\Black\\Queen.png");
+		whiteQueen = parent.loadImage("Image\\White\\Queen.png");
+	}
+
+	public void draw() {
+		if (moving) {
+			if (white) {
+				parent.image(whiteQueen, parent.mouseX - length / 2, parent.mouseY - length / 2, length, length);
+			} else {
+				parent.image(blackQueen, parent.mouseX - length / 2, parent.mouseY - length / 2, length, length);
+			}
+		} else {
+			if (white) {
+				parent.image(whiteQueen, x, y, length, length);
+			} else {
+				parent.image(blackQueen, x, y, length, length);
+			}
+		}
+	}
+}
+
+class King extends Piece {
+
+	PImage blackKing;
+	PImage whiteKing;
+
+	public King(int i, int j, int length, boolean white, PApplet p) {
+		super(i, j, length, white, p);
+		blackKing = parent.loadImage("Image\\Black\\King.png");
+		whiteKing = parent.loadImage("Image\\White\\King.png");
+	}
+
+	public void draw() {
+		if (moving) {
+			if (white) {
+				parent.image(whiteKing, parent.mouseX - length / 2, parent.mouseY - length / 2, length, length);
+			} else {
+				parent.image(blackKing, parent.mouseX - length / 2, parent.mouseY - length / 2, length, length);
+			}
+		} else {
+			if (white) {
+				parent.image(whiteKing, x, y, length, length);
+			} else {
+				parent.image(blackKing, x, y, length, length);
 			}
 		}
 	}
